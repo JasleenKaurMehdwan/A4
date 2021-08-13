02084f27edc0215a587850bc0a9dc1e35945139b
Jasleen Kaur <65685977+JasleenKaurMehdwan@users.noreply.github.com>
2021-08-13 19:18:24 -0400

    Add files via upload

---
+++ assets/Data/kaur8637Data.ts
@@ -0,0 +1,11 @@
+import { Type } from '../../app/type';
+
+export const MYINFO: Type = {
+  student_id: 991587295,
+  student_name: 'Jasleen Kaur',
+  student_login_name: 'Kaur8637',
+  student_campus: 'Davis',
+  assignment_title: 'Assignment_04'
+};
+ 
+
